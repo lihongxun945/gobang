30f19a7e735b4f174bff350133d509fa83300316
lihongxun945 <lihongxun945@gmail.com>
2016-02-17 14:05:18 +0800

    add math.js

---
+++ js/math.js
@@ -0,0 +1,19 @@
+var threshold = 1.2;
+
+module.exports = {
+  greatThan: function(a, b) {
+    return a > b * threshold;
+  },
+  greatOrEqualThan: function(a, b) {
+    return a * threshold > b;
+  },
+  littleThan: function(a, b) {
+    return a * threshold < b;
+  },
+  littleOrEqualThan: function(a, b) {
+    return a < b * threshold;
+  },
+  equal: function(a, b) {
+    return (a * threshold > b) && (a < b * threshold);
+  }
+}
