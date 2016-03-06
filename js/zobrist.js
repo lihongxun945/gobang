15199e874912307415585123ffb59cb087b59f47
hongxun.lhx <hongxun.lhx@alibaba-inc.com>
2016-03-06 15:35:58 +0800

    add zobrist.js

---
+++ js/zobrist.js
@@ -0,0 +1,28 @@
+var R = require("./role.js");
+
+var Zobrist = function(size) {
+  this.size = size || 15;
+}
+
+Zobrist.prototype.init = function() {
+  this.com = [];
+  this.hum = [];
+  for(var i=0;i<this.size*this.size;i++) {
+    this.com.push(this._rand());
+    this.hum.push(this._rand());
+  }
+
+  this.code = this._rand();
+}
+
+Zobrist.prototype._rand = function() {
+  return Math.floor(Math.random() * 1000000000);  //再多一位就溢出了。。
+}
+
+Zobrist.prototype.go = function(x, y, role) {
+  var index = this.size * x + y;
+  this.code ^= (role == R.com ? this.com[index] : this.hum[index]);
+  return this.code;
+}
+
+module.exports = Zobrist;
---
+++ tests/test-zobrist.js
@@ -0,0 +1,43 @@
+var Zobrist = require("../js/zobrist.js");
+var assert = require('assert');
+
+describe('test evalute', function() {
+  it(`test Zobrist`, function() {
+    var z = new Zobrist();
+    z.init();
+    var code = z.code;
+    console.log(code);
+
+    console.log(z.go(1,1,1));
+
+    assert.notEqual(z.code, code);
+
+    console.log(z.go(1,1,1));
+
+    assert.equal(z.code, code);
+
+    console.log(z.go(1,2,2));
+
+    assert.notEqual(z.code, code);
+  });
+
+  it(`两种不同顺序的走法应该返回同一个code`, function() {
+    var z = new Zobrist();
+    z.init();
+    z.go(1,1,1);
+    z.go(1,2,2);
+    z.go(2,1,1);
+    z.go(2,2,2);
+    var code1 = z.code;
+
+    z.go(2,1,1);
+    z.go(2,2,2);
+    z.go(1,1,1);
+    z.go(1,2,2);
+
+    var code2 = z.code;
+
+    assert.notEqual(code1, code2);
+  });
+});
+
