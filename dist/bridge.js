3aa506e83260745abca64eb4d4b3fe31d4f95641
lihongxun945 <lihongxun945@gmail.com>
2016-03-11 11:45:38 +0800

    update

--- dist/bridge.js
+++ dist/bridge.js
@@ -763,7 +763,7 @@ var hasNeighbor = require("./neighbor.js");
 var S = require("./score.js");
 var config = require("./config.js");
 
-var evaluate = function(board, role, includeSelf) {
+var evaluate = function(board, role) {
   
   var max = - S.FIVE;
   var min = - S.FIVE;
@@ -772,9 +772,9 @@ var evaluate = function(board, role, includeSelf) {
   for(var i=0;i<board.length;i++) {
     for(var j=0;j<board[i].length;j++) {
       if(board[i][j] == R.empty) {
-        if(hasNeighbor(board, [i, j], 2, 1)) { //必须是有邻居的才行
-          max = Math.max(scorePoint(board, [i,j], role, includeSelf), max);
-          min = Math.max(scorePoint(board, [i,j], R.reverse(role), includeSelf), min);
+        if(hasNeighbor(board, [i, j], 2, 2)) { //必须是有邻居的才行
+          max = Math.max(scorePoint(board, [i,j], role), max);
+          min = Math.max(scorePoint(board, [i,j], R.reverse(role)), min);
         }
       }
     }
