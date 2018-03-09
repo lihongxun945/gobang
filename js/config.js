1805f96f8586ae672d377c314fc1724943026612
lihongxun945 <lihongxun945@gmail.com>
2018-03-10 00:53:14 +0800

    update count limit

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   searchDeep: 6,  //搜索深度
   deepDecrease: .8, //按搜索深度递减分数，为了让短路径的结果比深路劲的分数高
-  countLimit: 36, //gen函数返回的节点数量上限，超过之后将会按照分数进行截断
+  countLimit: 25, //gen函数返回的节点数量上限，超过之后将会按照分数进行截断
   checkmateDeep:  6,  //算杀深度
   log: false,
   cache: false,  //是否使用效率不高的置换表
