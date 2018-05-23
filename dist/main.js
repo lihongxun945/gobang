611869be5b007b4dad7d4a1a0adb296c735367b0
hongxun.li <hongxun.li@ge.com>
2018-05-23 12:11:16 +0800

    关掉缓存

--- dist/main.js
+++ dist/main.js
@@ -1,14 +1,14 @@
 (function(){function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s}return e})()({1:[function(require,module,exports){
 module.exports = {
   opening: true, // 使用开局库
-  searchDeep: 10,  //搜索深度
+  searchDeep: 8,  //搜索深度
   countLimit: 20, //gen函数返回的节点数量上限，超过之后将会按照分数进行截断
   timeLimit: 100, // 时间限制，秒
   vcxDeep:  5,  //算杀深度
   random: false,// 在分数差不多的时候是不是随机选择一个走
   log: true,
   // TODO: 目前开启缓存后，搜索会出现一些未知的bug
-  cache: true // 使用缓存, 其实只有搜索的缓存有用，其他缓存几乎无用。因为只有搜索的缓存命中后就能剪掉一整个分支，这个分支一般会包含很多个点。而在其他地方加缓存，每次命中只能剪掉一个点，影响不大。
+  cache: false // 使用缓存, 其实只有搜索的缓存有用，其他缓存几乎无用。因为只有搜索的缓存命中后就能剪掉一整个分支，这个分支一般会包含很多个点。而在其他地方加缓存，每次命中只能剪掉一个点，影响不大。
 }
 
 },{}],2:[function(require,module,exports){
