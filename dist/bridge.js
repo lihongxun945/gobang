ceab1bd220dc09e94c1fed593e94ac6a1377f2cd
hongxun.li <hongxun.li@ge.com>
2018-06-04 14:53:49 +0800

    修复了开局库的错误

--- dist/bridge.js
+++ dist/bridge.js
@@ -1528,7 +1528,7 @@ var puyue = function (board) {
 
 var match = function (board) {
   var s = board.steps;
-  if (s[0] !== 1) return false
+  if (board.board[s[0][0]][s[0][1]] !== 1) return false
   if (s.length > 2) return false
   if (math.containPoint([[6,7],[7,6],[8,7],[7,8]], s[1])) return huayue(board)
   else if (math.containPoint([[6,6],[8,8],[8,6],[6,8]], s[1])) return puyue(board)
