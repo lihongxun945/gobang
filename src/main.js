a19d8fb4338d205403d386fa6190530c81281474
lihongxun945 <lihongxun945@gmail.com>
2018-07-05 22:50:28 +0800

    disable sw

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store/index.js'
-import './registerServiceWorker'
+//import './registerServiceWorker'
 
 import 'weui/dist/style/weui.min.css'
 import './reset.scss'
